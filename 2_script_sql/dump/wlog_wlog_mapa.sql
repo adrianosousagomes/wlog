b33c4e8f5c3e1c91ed81a2e28ea8de1b2f5ab3d1
Adriano S. Gomes <kapiturasp@gmail.com>
2015-11-29 17:36:52 -0200

    Commit inicial.

---
+++ 2_script_sql/dump/wlog_wlog_mapa.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE  IF NOT EXISTS `wlog` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `wlog`;
+-- MySQL dump 10.13  Distrib 5.6.23, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: wlog
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.8-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `wlog_mapa`
+--
+
+DROP TABLE IF EXISTS `wlog_mapa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wlog_mapa` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `data_inclusao` datetime NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wlog_mapa`
+--
+
+LOCK TABLES `wlog_mapa` WRITE;
+/*!40000 ALTER TABLE `wlog_mapa` DISABLE KEYS */;
+INSERT INTO `wlog_mapa` VALUES (1,'2015-11-26 15:28:39','SP');
+/*!40000 ALTER TABLE `wlog_mapa` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-11-29 17:20:14
